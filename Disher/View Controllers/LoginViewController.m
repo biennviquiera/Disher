a813924869416c927566e286d2608d2b7a66970d
Bienn <biennviquiera@gmail.com>
2022-07-07 16:51:45 -0700

    added loginviewcontroller back which was deleted for some reason

---
+++ Disher/View Controllers/LoginViewController.m
@@ -0,0 +1,97 @@
+//
+//  LoginViewController.m
+//  Disher
+//
+//  Created by Bienn Viquiera on 7/5/22.
+//
+
+#import "LoginViewController.h"
+@import Parse;
+
+
+@interface LoginViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+}
+
+//BUTTON ACTIONS
+
+- (IBAction)didTapLogin:(id)sender {
+    [self loginUser];
+}
+
+- (IBAction)didTapSignup:(id)sender {
+    [self registerUser];
+}
+
+//HELPER METHODS
+
+- (void) loginUser {
+    NSString *username = self.usernameField.text;
+    NSString *password = self.passwordField.text;
+    
+    [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+        if (error != nil) {
+            NSString *errorString = [error userInfo][@"error"];
+            
+            UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Error logging in"
+                                                                           message:errorString
+                                                                    preferredStyle:UIAlertControllerStyleAlert];
+             
+            UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"Try again"
+                                                                    style:UIAlertActionStyleDefault
+                                                                  handler:^(UIAlertAction * action) {}];
+            [alert addAction:defaultAction];
+            [self presentViewController:alert animated:YES completion:nil];
+        } else {
+            NSLog(@"User logged in successfully");
+            [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+        }
+    }];
+}
+
+- (void) registerUser {
+    PFUser *newUser = [PFUser user];
+    newUser.username = self.usernameField.text;
+    newUser.password = self.passwordField.text;
+    
+    [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
+        if (error) {            NSString *errorString = [error userInfo][@"error"];
+            
+            UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Error signing up"
+                                                                           message:errorString
+                                                                    preferredStyle:UIAlertControllerStyleAlert];
+             
+            UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"Try again"
+                                                                    style:UIAlertActionStyleDefault
+                                                                  handler:^(UIAlertAction * action) {}];
+             
+            [alert addAction:defaultAction];
+            [self presentViewController:alert animated:YES completion:nil];
+        }
+        else {
+            [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+        }
+    }];
+}
+
+
+/*
+#pragma mark - Navigation
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
