d0cd6bd9d7cbbae23d22c69778b903072eeaf882
Bienn Viquiera <biennviquiera@gmail.com>
2022-08-03 16:31:03 -0700

    condensed detail view VCs into a single one by changing the segue. Deleted old view controller classes

--- Disher/View Controllers/ListContentViewController.m
+++ Disher/View Controllers/ListContentViewController.m
@@ -89,14 +89,7 @@
     }];
 }
 
-
-
-#pragma mark - Navigation
-//TODO: Pass data from id lookup
-// In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
     if ([[segue identifier] isEqualToString:@"listRecipeSegue"]) {
         RecipeInListViewController *newVC = [segue destinationViewController];
         newVC.passedRecipe = sender;
